5f4fcb92599353b13ac4ba08b358bec3be929a96
Ryan-shamu-YT <83490776+Ryan-shamu-YT@users.noreply.github.com>
2024-04-14 17:11:56 +0200

    Update __init__.py

--- scrathon_payments/__init__.py
+++ scrathon_payments/__init__.py
@@ -1,1 +1,30 @@
-...
+import json
+import re
+import requests
+
+
+class Scrathon():
+
+    def __init__(self, username):
+        self.username = str(username)
+
+        print("Started Scrathon as {}! All funds earned will go to mentioned user".format(username))
+
+    def purchase(self, price, user):
+        request = requests.post("https://scrathon.justablock.online/transaction", json={
+            "price": price,
+            "buyer": user,
+            "seller": self.username
+        })
+
+        return request.content
+    
+    def purchasecheck(self, price, user):
+        request = requests.post("https://scrathon.justablock.online/checkpurchase", json={
+            "price": price,
+            "buyer": user,
+            "seller": self.username
+        })
+
+        return request.content
+
