4655710c32aab3ee4c2b3fc74af937a1b543136f
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-04-14 16:43:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='scrathon-payments',
+    version='0.1',
+    author='NAME',
+    author_email='EMAIL',
+    description='DESCRIPTION',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ryan-shamu-YT/ScrathonPayments',
+    packages=find_packages(exclude=[]),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['scratch', 'api'],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.6',
+)
