20c6730f447f31b6856c3a9b06f661fa0fbb8ffb
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2024-05-24 18:23:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='scrathon-payments',
-    version='0.2.4',
+    version='0.2.5',
     author='Ryan_shamu',
     author_email='Ryanshamu418@gmail.com',
     description='API Wrapper for ScrathonPayments',
