29f6a5becc7cbbdef47067bd7e7a8758bee3d970
Ryan-shamu-YT <ryanshamu418@gmail.com>
2024-05-24 08:57:06 +0200

    Update __init__.py

--- ScrathonPayments/__init__.py
+++ ScrathonPayments/__init__.py
@@ -1,9 +1,10 @@
 import scratchattach as scratch3
 import requests
 
-class Scrathon():
+class Scrathon:
 
-    def __init__(self, username):
+    def __init__(self, username, client: scratch3.CloudRequests):
+        self.client = client
         self.username = "None"
         try:
             scratch3.get_user(username)
@@ -21,20 +22,21 @@ class Scrathon():
         if self.username != "None":
             print("Started Scrathon as {}! All funds earned will go to mentioned user".format(self.username))
 
-    def purchase(self, price, user):
-        request = requests.post("http://45.140.188.129:6623/transaction", json={
-            "price": price,
-            "buyer": user,
-            "seller": self.username
-        })
+        @client.request
+        def purchase(price):
+            request = requests.post("http://45.140.188.129:6623/transaction", json={
+                "price": price,
+                "buyer": client.get_requester(),
+                "seller": self.username
+            })
 
-        return request.json()
-    
-    def purchasecheck(self, price, user):
-        request = requests.post("http://45.140.188.129:6623/checkpurchase", json={
-            "price": price,
-            "buyer": user,
-            "seller": self.username
-        })
+            return request.json()
+        @client.request
+        def purchasecheck(price):
+            request = requests.post("http://45.140.188.129:6623/checkpurchase", json={
+                "price": price,
+                "buyer": client.get_requester(),
+                "seller": self.username
+            })
 
-        return request.json()
+            return request.json()
